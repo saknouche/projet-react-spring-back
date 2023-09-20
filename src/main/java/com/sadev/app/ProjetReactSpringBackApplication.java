43cddbf44358ce72d2a2ed97b451e98313fe8b1a
saknouche <developpeur.web90@gmail.com>
2023-09-20 10:03:01 +0200

    first commit

---
+++ src/main/java/com/sadev/app/ProjetReactSpringBackApplication.java
@@ -0,0 +1,13 @@
+package com.sadev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetReactSpringBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetReactSpringBackApplication.class, args);
+	}
+
+}
